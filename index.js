82985cff054ca11aab69db07b43ce628eb948213
Aiko2388 <92234638+Aiko2388@users.noreply.github.com>
2021-10-09 19:29:30 -0400

    Update Index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+const { configtoken } = require('./config.json')
 const aoijs = require('aoi.js')
 const bot = new aoijs.Bot({
-    token: PizzaTokenPrivate,
+    token: configtoken,
     prefix: ['$getServerVar[prefix]'],
 })
 
@@ -13,15 +14,3 @@ database: mongoose.mongo
 
 //Command Handler
 bot.loadCommands(`./System/`)
-
-//Variable Handler
-const variables = require('./Security/variables.js');
-bot.variables(variables);
-
-//Event Listeners Handler
-require('./Security/EventsListener')(bot)
-
-//Status Handler
-const status = require('./Security/Status Control/status.js');
-bot.status(status);
-
